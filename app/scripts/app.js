27691ebb83dac8174caaf6b76fb66b130518a44a
Keichi Takahashi <keichi.t@me.com>
2015-10-27 20:12:24 +0900

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sdnMpiConsoleApp
+ * @description
+ * # sdnMpiConsoleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sdnMpiConsoleApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdnMpiConsoleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sdnMpiConsoleApp
+ */
+angular.module('sdnMpiConsoleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
